87ab2a5616dd5ed232e9bdbe8b9267f9bdb82b68
Ramon Lacava <ramonrune@gmail.com>
2019-01-06 16:20:01 -0200

    Update

--- ride_tracker/src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ ride_tracker/src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -12,7 +12,23 @@ import com.pluralsight.model.Ride;
 import org.junit.Test;
 
 public class RestControllerTest {
+	
+	  	   
+	@Test()
+	public void testCreateRide() {
+		RestTemplate restTemplate = new RestTemplate();
 
+		
+		Ride ride = new Ride();
+		ride.setName("Passeio até americana");
+		ride.setDuration(15);
+		
+		restTemplate.put("http://localhost:8080/ride_tracker/ride", ride);
+		
+	
+	}
+	
+/*
 	@Test(timeout=3000)
 	public void testGetRides() {
 		RestTemplate restTemplate = new RestTemplate();
@@ -26,5 +42,6 @@ public class RestControllerTest {
 		for (Ride ride : rides) {
 			System.out.println("Ride name: " + ride.getName());
 		}
-	}
+	}*/
+	
 }
